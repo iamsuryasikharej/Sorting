fbb0a19bd822dad96bba9d133415551d47752a8b
iamsuryasikharej <iamssrofficial@gmail.com>
2019-01-29 13:34:59 +0530

    fizzbizz

---
+++ Example24.java
@@ -0,0 +1,24 @@
+class a{
+	public void fizzbizz()
+	{
+	for(int i=1;i<=100;i++)
+	{
+	if (i%3==0)
+	{
+	System.out.println("fizz"+i);
+	}
+	else if(i%5==0)
+	{
+	System.out.println("bizz"+i);
+	}
+	else
+	continue;
+	}
+	}
+}
+class Example24{
+	public static void main(String [] args){
+	a obj=new a();
+	obj.fizzbizz();
+	}
+}
